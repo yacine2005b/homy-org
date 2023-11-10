71ecade0d2f5ec2aa9feec8746129acb616f4017
yacine1802 <yybouz1802@gmail.com>
2023-11-10 16:15:19 +0100

    finished login

--- controlers/blogControler.js
+++ controlers/blogControler.js
@@ -10,7 +10,15 @@ const handleErrors = (err) => {
 		errors.email = 'this email is already registered'
 		return errors
 	}
+	// incorrect email
+	if (err.message === 'incorrect email') {
+		errors.email = 'That email is not registered';
+	}
 
+	// incorrect password
+	if (err.message === 'incorrect password') {
+		errors.password = 'That password is incorrect';
+	}
 	//validation errors
 	if (err.message.includes('user validation failed')) {
 		Object.values(err.errors).forEach(({ properties }) => {
@@ -82,9 +90,12 @@ const login_post = async (req, res) => {
 	const { email, password } = req.body
 	try {
 		const user = await User.login(email, password);
+		const token = createToken(user._id)
+		res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
 		res.status(200).json({ user: user._id });
 	} catch (err) {
-		res.status(400).json({});
+		const errors = handleErrors(err)
+		res.status(400).json({ errors });
 	}
 }
 
